9f3e517934031d64963816d9ca96101c34743d79
Julián Ceballos <cristianjulianceballos@gmail.com>
2010-10-22 15:11:23 -0700

    Animaciones en la presentación

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ init = function(){
     img.witdh = window.innerWidth-100;
     slides = document.getElementsByTagName("div");
     actual = 0;
+    slides[actual].style.MozTransform = "scale(1)";
 }
 
 keyDetect = function(key){
@@ -17,28 +18,46 @@ keyDetect = function(key){
     }
 }
 
+apper = function(){
+    setInterval(function(){
+        slides[actual].style.MozTransform = "scale(1)";
+    },500);
+}
+
+disapper = function(){
+    slides[actual].style.MozTransform = "scale(0)";
+}
+
 slideRight = function(){
     if(actual<(slides.length-1) && actual>=0){
+        disapper();
         slides[actual].style.display = "none";
         actual++;
         slides[actual].style.display = "";
+        apper();
     }
     else{
+        disapper();
         slides[actual].style.display = "none";
         actual = 0;
         slides[actual].style.display = "";
+        apper();
     }
 }
 
 slideLeft = function(){
     if(actual<=(slides.length-1) && actual>0){
+        disapper();
         slides[actual].style.display = "none";
         actual--;
         slides[actual].style.display = "";
+        apper();
     }
     else{
+        disapper();
         slides[actual].style.display = "none";
         actual = (slides.length-1);
         slides[actual].style.display = "";
+        apper();
     }
 }
