f36d95506c4ad6c79e68e43fc16bcbe156f3745d
JCeb <julian@pikhub.com>
2012-02-22 17:58:23 -0600

    transitions working

--- script.js
+++ script.js
@@ -1,9 +1,29 @@
 var actual = 0;
 var slides;
 
+/*
+var transitions = {
+    "twister" : {
+        "startPoint" : "scale(0) rotate(5deg)",
+        "incoming" : "scale(1) rotate(360deg)",
+        "outgoing" : "scale(0) rotate(5deg)",
+    },
+    "zoomIn" : {
+        "startPoint" : "scale(0) rotate(0deg)",
+        "incoming" : "scale(1) rotate(0deg)",
+        "outgoing" : "scale(0) rotate(0deg)",
+    },
+    "rightToLeft" : {
+        "startPoint" : "scale(100) rotate(0deg)",
+        "incoming" : "scale(1) rotate(0deg)",
+        "outgoing" : "scale(0) rotate(0deg)",
+    }
+}
+*/
+
 init = function(){
     slides = document.getElementsByTagName("section");
-    apper();
+    appear();
 }
 
 keyDetect = function(key){
@@ -15,54 +35,52 @@ keyDetect = function(key){
     }
 }
 
-apper = function(){
-    setInterval(function(){
-        slides[actual].style.MozTransform = "scale(1) rotate(360deg)";
-        slides[actual].style.WebkitTransform = "scale(1) rotate(360deg)";
-        slides[actual].style.OTransform = "scale(1) rotate(360deg)";
-    },500);
+appear = function(){
+    slide = slides[actual];
+    transition = transitions_in[slide.getAttribute("data-transition-in")];
+    transition(slide);
     bodyHeight = document.body.scrollHeight;
     slideHeight = slides[actual].clientHeight;
     marginTop = bodyHeight/2 - slideHeight/2;
     slides[actual].style.marginTop = marginTop.toString()+'px';
+    return;
 }
 
-disapper = function(){
-    slides[actual].style.MozTransform = "scale(0) rotate(10deg)";
-    slides[actual].style.WebkitTransform = "scale(0) rotate(10deg)";
-    slides[actual].style.OTransform = "scale(0) rotate(10deg)";
+disappear = function(){
+    slide = slides[actual];
+    return;
 }
 
 slideRight = function(){
     if(actual<(slides.length-1) && actual>=0){
-        disapper();
+        disappear();
         slides[actual].style.display = "none";
         actual++;
         slides[actual].style.display = "";
-        apper();
+        appear();
     }
     else{
-        disapper();
+        disappear();
         slides[actual].style.display = "none";
         actual = 0;
         slides[actual].style.display = "";
-        apper();
+        appear();
     }
 }
 
 slideLeft = function(){
     if(actual<=(slides.length-1) && actual>0){
-        disapper();
+        disappear();
         slides[actual].style.display = "none";
         actual--;
         slides[actual].style.display = "";
-        apper();
+        appear();
     }
     else{
-        disapper();
+        disappear();
         slides[actual].style.display = "none";
         actual = (slides.length-1);
         slides[actual].style.display = "";
-        apper();
+        appear();
     }
 }
