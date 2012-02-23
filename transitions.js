f147cc747fa7fc5d908e6887d3e0da4e5cb7d2f6
JCeb <julian@pikhub.com>
2012-02-23 13:37:52 -0600

    new effect -> fallIn :)

--- transitions.js
+++ transitions.js
@@ -82,8 +82,44 @@ var fadeIn = function(slide){
     }, true);
 }
 
+var fallIn = function(slide){
+    property = "margin, opacity";
+    duration = "1s";
+    slide.style.transitionProperty = property;
+    slide.style.transitionDuration = duration;
+    slide.style.MozTransitionProperty = property;
+    slide.style.MozTransitionDuration = duration;
+    slide.style.WebkitTransitionProperty = property;
+    slide.style.WebkitTransitionDuration = duration;
+    slide.style.OTransitionProperty = property;
+    slide.style.OTransitionDuration = duration;
+    slide.style.marginTop = "-2000px";
+    slide.style.opacity = 1;
+    slide.style.display = "";
+    animation = setInterval(function(){
+        bodyHeight = document.body.scrollHeight;
+        slideHeight = slide.clientHeight;
+        marginTop = bodyHeight/2 - slideHeight/2;
+        slide.style.marginTop = marginTop.toString()+'px';
+        slide.style.opacity = 1;
+    },500);
+    slide.addEventListener("webkitTransitionEnd", function(){
+      clearInterval(animation);
+    }, true);
+    slide.addEventListener("transitionend", function(){
+      clearInterval(animation);
+    }, true);
+    slide.addEventListener("oTransitionEnd", function(){
+      clearInterval(animation);
+    }, true);
+    slide.addEventListener("MSTransitionEnd", function(){
+      clearInterval(animation);
+    }, true);
+}
+
 var transitions_in = {
     "twister" : twisterIn,
     "zoomIn" : zoomIn,
     "fadeIn" : fadeIn,
+    "fallIn" : fallIn,
 }
